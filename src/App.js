ec4b04978c7afb4044882db14102a2476fdb1e59
Ellie <camera20120109@gmail.com>
2023-11-20 22:55:27 +0800

    revise input

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import Bubble from "./Bubble.jpg";
 import ENFP from"./ENFP.jpg";
 import ENTP from"./ENTP.jpg";
+import react, {useStatus} from 'react';
+
 
 
 
 
 function App() {
+
+  const MBTIType = () => {
+    const option = ["INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ",
+                    "ISTP", "ISFP", "ESTP", "ESFP" ]
+  }
+  
+
+
+
   return (
     <div className="App">
       <header style= {{ width: '100%', height: '284px', flexShrink: '0', alignItems: 'center', flexDirection: 'column',}}>
@@ -19,13 +30,12 @@ function App() {
             <header>Male</header>
             <div style={{display: 'flex', alignItems: 'center', borderRadius: '12px', border: '1px solid #7F9EBD', background: 'var(--default-white, #FFF)',}}>
                     <div style = {{width: '324px',height: '265px',flexShrink: '0', }}>
-                    {/* background: `url('./ENFP.jpg'), lightgray 50% / cover no-repeat` */}
                       <img src={ENFP} alt = "ENFP" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '12px',}}></img>
                     </div>
-
                     <div style= {{display: 'flex', width: '348px',padding: '33px 45px',flexDirection: 'column',justifyContent: 'center', alignItems: 'center', gap: '33px', alignSelf: 'stretch',}}>
                       
-                      <input style = {{width: '300px', height: '30px',borderRadius: '6px', border: '1px solid #58674F', opacity: '0.5', background: 'var(--white, #FFF)',}}/>
+                      <select style = {{width: '300px', height: '30px',borderRadius: '6px', border: '1px solid #58674F', opacity: '0.5', background: 'var(--white, #FFF)',}}
+                           />
                       <input style = {{width: '300px', height: '30px',borderRadius: '6px', border: '1px solid #58674F', opacity: '0.5', background: 'var(--white, #FFF)',}}/>
                       <input style = {{width: '300px', height: '30px',borderRadius: '6px', border: '1px solid #58674F', opacity: '0.5', background: 'var(--white, #FFF)',}}/>
                     
@@ -35,24 +45,7 @@ function App() {
           </div>
 
 
-          <div style = {{display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', gap: '33px',}}>
-            <header>Female</header>
-            <div style={{display: 'flex', alignItems: 'center', borderRadius: '12px', border: '1px solid #7F9EBD', background: 'var(--default-white, #FFF)',}}>
-                    <div style = {{width: '324px',height: '265px',flexShrink: '0', }}>
-                    {/* background: `url('./ENFP.jpg'), lightgray 50% / cover no-repeat` */}
-                      <img src={ENTP} alt = "ENFP" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '12px',}}></img>
-                    </div>
-
-                    <div style= {{display: 'flex', width: '348px',padding: '33px 45px',flexDirection: 'column',justifyContent: 'center', alignItems: 'center', gap: '33px', alignSelf: 'stretch',}}>
-                      
-                      <input style = {{width: '300px', height: '30px',borderRadius: '6px', border: '1px solid #58674F', opacity: '0.5', background: 'var(--white, #FFF)',}}/>
-                      <input style = {{width: '300px', height: '30px',borderRadius: '6px', border: '1px solid #58674F', opacity: '0.5', background: 'var(--white, #FFF)',}}/>
-                      <input style = {{width: '300px', height: '30px',borderRadius: '6px', border: '1px solid #58674F', opacity: '0.5', background: 'var(--white, #FFF)',}}/>
-                    
-                    </div>
-            </div>
-
-          </div>
+          
 
       <footer>
 
